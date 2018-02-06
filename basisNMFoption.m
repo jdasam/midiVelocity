fcd1b8b3e3fe2b677f3578e5f1ff43fc7dc6a842
Dasaem Jeong - iMac <jdasam@kaist.ac.kr>
2018-02-06 21:22:38 +0900

    typo

--- basisNMFoption.m
+++ basisNMFoption.m
@@ -82,7 +82,7 @@ else
         B = Bnew;
         G = Gnew;
 
-        B = beataNormC(B,1);
+        B = betaNormC(B,1);
         Xhat = (B.^basicParameter.spectrumMode * G.^basicParameter.spectrumMode) .^ (1/basicParameter.spectrumMode) + eps;
 
         if strcmp(basicParameter.spectrumMode, 'linear')
