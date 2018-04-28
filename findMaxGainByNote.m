2fbb803483587bf1e78570be7fd5b9825a079ce1
jdasam <jdasam@kaist.ac.kr>
2018-04-28 23:44:40 +0900

    updatedFind modified

--- findMaxGainByNote.m
+++ findMaxGainByNote.m
@@ -124,7 +124,7 @@ function offset = calOffset(midiNote, midiEntire, basicParameter)
     if nextNote
         nextOnsetFrame = onsetTime2frame(nextNote(6), basicParameter);
         if offset>nextOnsetFrame
-            offset = nextOnsetFrame -1 + basicParameter.maxIndexFromOnset;
+            offset = min(nextOnsetFrame -1 + basicParameter.maxIndexFromOnset, offset);
         end
     end
    
