26658cbe26a3b33de3015f37d8589bdb536141c2
Dasaem Jeong - iMac <jdasam@kaist.ac.kr>
2018-04-29 00:32:34 +0900

    Revert "try to revert"
    This reverts commit 2b1f22a744dc357cd3d7d3ec81661bc4962a3e5b.

--- basisNMFoption.m
+++ basisNMFoption.m
@@ -28,7 +28,7 @@ elseif strcmp(dataSource, 'data')
         end
     end
 end
-% B = betaNormC(B, basicParameter.beta);
+B = betaNormC(B, basicParameter.beta);
 
 if basicParameter.softConstraint && basicParameter.useSoftConstraintInScale
     constraintMatrix = G;
