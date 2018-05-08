f7544bed0c2d0595d4636cb40d823d22c34738a7
Dasaem Jeong - iMac <jdasam@kaist.ac.kr>
2018-05-08 14:59:49 +0900

    midi2midi compare ignore pitch-mismatched notes

--- midiMatAlign.m
+++ midiMatAlign.m
@@ -11,7 +11,7 @@ for i=1:length(midiScore)
     end
     
     for j=1:length(candidateIndex)
-        if strcmp(alignResult(candidateIndex(j),9), num2str(midiScore(i,4)) ) && ~strcmp(alignResult(candidateIndex(j), 2) , '-1')
+        if strcmp(alignResult(candidateIndex(j),4), num2str(midiScore(i,4)) ) && ~strcmp(alignResult(candidateIndex(j), 2) , '-1')
             midiAligned(i,4) = str2double(alignResult(candidateIndex(j),4));
             midiAligned(i,5) = str2double(alignResult(candidateIndex(j),5));
             midiAligned(i,6) = str2double(alignResult(candidateIndex(j),2));
